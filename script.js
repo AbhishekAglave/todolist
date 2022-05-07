6ff04f05d5748c64aa3d9b7d3b92daf509e8b288
AnimeBoyYt <abhishekaglawe00@gmail.com>
2022-05-08 01:58:46 +0530

    saving on local storage added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let task = document.querySelector('#task-input');
 let add = document.querySelector('#todo-form');
 let list = document.querySelector('#todo-list');
-
+list.innerHTML = localStorage.getItem('tasklistHTML');
 function addTask(event) {
     event.preventDefault();
     if (task.value == "") {
@@ -18,19 +18,25 @@ function addTask(event) {
 
         let donebtn = document.createElement('button');
         donebtn.innerText = "Done";
-        donebtn.setAttribute('id', 'donebtn');
+        donebtn.classList.add('donebtn');
         donebtn.setAttribute('onclick', 'this.parentNode.classList.toggle("strike-through")');
         newTask.appendChild(donebtn);  // appending done btn to li element 
 
         let deletebtn = document.createElement('button');
         deletebtn.innerText = "Delete";
-        deletebtn.setAttribute('id', 'deletebtn');
+        deletebtn.classList.add('deletebtn');
         deletebtn.setAttribute('onclick', 'this.parentNode.remove()');
         newTask.appendChild(deletebtn); // appending delete btn to li element
 
         task.value = ""; // clears the text from input field after the task is added
         task.setAttribute('placeholder', '');
+
+        updateList(); // updates the local storage;
     }
+}
+
+function updateList(){
+    localStorage.setItem('tasklistHTML', list.innerHTML);
 }
 
 add.addEventListener('submit', addTask);
